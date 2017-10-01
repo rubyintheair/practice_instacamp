2ef04af8099f15d08ff9bab599f06a08bfa30a6c
Quy <quy.nguyenngoctp@gmail.com>
2017-10-01 12:35:29 +0700

    first seed datat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Photo.create!(url: "https://media.sheis.vn/images/image010(600).jpg", username: "Pham Bang Bang 2", likes_count: 20)
+Photo.create!(url: "http://2sao.vietnamnetjsc.vn/2016/09/18/13/22/201605131047286630582881072660737600x871.jpg", username: "Truong Hinh Du", likes_count: 30)
+Photo.create!(url: "https://cdn.thinglink.me/api/image/423227017190703104/1240/10/scaletowidth", username: "Emma", likes_count: 12)
+Photo.create!(url: "http://media1.santabanta.com/full1/Global%20Celebrities(F)/Emma%20Watson/emma-watson-42a.jpg", username: "Emma 2", likes_count: 15)
+Photo.create!(url: "http://images2.fanpop.com/image/photos/8900000/Emma-Watson-emma-watson-8948941-500-313.jpg", username: "Emma 3", likes_count: 10)
